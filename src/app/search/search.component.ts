e56111e6f858cf823232cc8b9212ebb507f0b96a
mingxin-yu <mingxin.y@zymebalanz.com>
2024-03-04 21:42:20 -0800

    finish search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,19 +7,32 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import {Food} from "../models/food.interface"
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatDividerModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatDividerModule, MatButtonModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  isSearch: boolean = false;
+  isEdit: boolean = false;
+
   foods: Food[] = [
     {value: 'steak-0', viewValue: 'Steak'},
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'},
   ];
+
+  toggleSearch(){
+    this.isSearch = !this.isSearch;
+  }
+
+  toggleEdit(){
+    this.isEdit = !this.isEdit;
+  }
+
 }
 
